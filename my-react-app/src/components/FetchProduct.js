7d019546353f14a49868dded5bac3c5cb1354a44
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-01-23 23:18:04 -0800

    get API upon search text change added

--- my-react-app/src/components/FetchProduct.js
+++ my-react-app/src/components/FetchProduct.js
@@ -1,30 +1,55 @@
 import React, { Component } from 'react';
 import './FetchProduct.css';
+import axios from "axios";
+
 
 class FetchProduct extends Component {
 
 state = {
+  searchText: "",
   loading: true,
+  apiUrl: "http://makeup-api.herokuapp.com/api/v1/products.json",
   items: []
-}
+};
 
-async componentDidMount() {
- const url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl&product_type=lipstick";
-    const response = await fetch(url);
-    const data = await response.json();
-   this.setState({ items: data, loading: false });
-  }
+onTextChange = e =>{
+  const val = e.target.value;
+  this.setState({[e.target.name]: val }, () => {
+    if(val === ""){
+      this.setState({items: []});
+    } else {
+      axios
+      .get(
+        `${this.state.apiUrl}?product_type=${this.state.searchText}`
+      )
+      .then(res => this.setState({items: res.data}))
+      .catch(err => console.log(err));
+    }
+  });
+};
+
+// async componentDidMount() {
+//  const url = "http://makeup-api.herokuapp.com/api/v1/products.json?brand=covergirl&product_type=lipstick";
+//     const response = await fetch(url);
+//     const data = await response.json();
+//    this.setState({ items: data, loading: false });
+//   }
 
   render() {
-    if(this.state.loading) {
-      return <div>loading...</div>
-    }
+    // if(this.state.loading) {
+    //   return <div>loading...</div>
+    // }
 
-    if(!this.state.items) {
-      return <div>didn't get any product</div>
-    }
+    // if(!this.state.items) {
+    //   return <div>didn't get any product</div>
+    // }
+
+    
     return (
-       <div className="layout">
+       <div className="search">
+      <input name="searchText" className="search-bar" type="text" placeholder="Search" onChange={this.onTextChange}/> 
+      
+        <div className="layout">
         {this.state.items.map(item => (
         <div key={item.id} className="item">
          <img src={item.image_link} />
@@ -34,11 +59,9 @@ async componentDidMount() {
           </div>
         ))}
          
- 
-      
 
         </div>
-        
+        </div>
     );
   }
 }
