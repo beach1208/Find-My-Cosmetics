a0ad6616066f6b3693cf31336f52373b55b4e00b
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-01-24 11:53:31 -0800

    repo name changed

---
+++ my-react-app/src/App.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import logo from './makeup.svg';
+import './App.css';
+import FetchProduct from './components/FetchProduct';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="title-name">Find My Cosmetics</h1>
+     
+          
+        </header>
+          <FetchProduct/>
+      </div>
+    );
+  }
+}
+
+export default App;
