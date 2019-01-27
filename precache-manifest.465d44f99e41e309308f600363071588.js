b3812dd33bfa4d8b27da7ec47731702b51cd7e83
Nagisa Kojima <nk.bg.1208@gmail.com>
2019-01-26 23:40:15 -0800

    Updates

---
+++ precache-manifest.465d44f99e41e309308f600363071588.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "031a725cafd65190239f",
+    "url": "/beach1208/Find-My-Cosmetics/static/css/main.754d0271.chunk.css"
+  },
+  {
+    "revision": "031a725cafd65190239f",
+    "url": "/beach1208/Find-My-Cosmetics/static/js/main.031a725c.chunk.js"
+  },
+  {
+    "revision": "cbb65d73829b0c0a105c",
+    "url": "/beach1208/Find-My-Cosmetics/static/js/1.cbb65d73.chunk.js"
+  },
+  {
+    "revision": "a34276f323cd055e11ab",
+    "url": "/beach1208/Find-My-Cosmetics/static/js/runtime~main.a34276f3.js"
+  },
+  {
+    "revision": "8495d5d0d3a47d50c30b25cdc60620cb",
+    "url": "/beach1208/Find-My-Cosmetics/static/media/makeup.8495d5d0.svg"
+  },
+  {
+    "revision": "67edcff8f089d9f2040bae3eb463d29e",
+    "url": "/beach1208/Find-My-Cosmetics/static/media/noimg.67edcff8.svg"
+  },
+  {
+    "revision": "1b8f7bf98f555171a8fd5cb3285cdf67",
+    "url": "/beach1208/Find-My-Cosmetics/index.html"
+  }
+];
